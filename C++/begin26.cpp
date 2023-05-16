c0c1240a236257c1914d3d48c3e3854c576cf269
BekzhanovR <bekzhanovofficial@gmail.com>
2023-05-16 21:56:54 +0500

    codes

--- C++/begin26.cpp
+++ C++/begin26.cpp
@@ -2,8 +2,13 @@
 // Sana: 15.05.2023
 
 #include <iostream>
+#include <cmath>
 using namespace std;
 
 int main(){
-    
+    long long y,x;
+    cout << "X="; cin >> x;
+    y = 4*pow((x-3),6)-7*pow((x-3),3)+2;
+    cout << "Y=" << y;
+    return 0;
 }
