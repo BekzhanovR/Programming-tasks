2c3a106e4d7ab4db4fd213844a66ab5aef78a87b
BekzhanovR <bekzhanovofficial@gmail.com>
2023-08-11 17:28:00 +0500

    scripts

---
+++ Begin/33.py
@@ -0,0 +1,7 @@
+x = int(input())
+a = int(input())
+y = int(input())
+price_of_piece = a/x
+price_of_ykg = price_of_piece * y
+print(price_of_piece)
+print(price_of_ykg)
---
+++ Begin/35.py
@@ -0,0 +1,6 @@
+v = int(input())
+u = int(input())
+t1 = int(input())
+t2 = int(input())
+s = v*t1+(v-u)*t2
+print(s)
