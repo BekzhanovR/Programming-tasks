57015c7d25360ee6c016d86025c68f8d3fb43b30
BekzahnovR <bekzhanovofficial@gmail.com>
2023-11-02 10:00:54 +0500

    Scripts

--- #01 Begin/21.py
+++ #01 Begin/21.py
@@ -1,15 +1,15 @@
-from math import *
+from math import sqrt
 x1 = int(input())
 y1 = int(input())
 x2 = int(input())
 y2 = int(input())
 x3 = int(input())
 y3 = int(input())
-a = sqrt(pow(x2-x1,2)+pow(y2-y1,2))
-b = sqrt(pow(x3-x1,2)+pow(y3-y1,2))
-c = sqrt(pow(x3-x2,2)+pow(y3-y2,2))
+a = sqrt(pow(x1-x2,2)+pow(y1-y2,2))
+b = sqrt(pow(x2-x3,2)+pow(y2-y3,2))
+c = sqrt(pow(x3-x1,2)+pow(y3-y1,2))
 p = a+b+c
-pp = p/2
-s = (pp*(pp-a)*(pp-b)*(pp-c))**(1/2)
-print(p)
-print(s)
+p_2 = p/2
+s = sqrt(p_2*(p_2-a)*(p_2-b)*(p_2-c))
+print(int(p))
+print(int(s))
